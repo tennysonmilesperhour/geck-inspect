ee6223ec2624bc294383bba58ec3c279acb93b49
Tennyson Taggart <tennysontaggart@gmail.com>
2026-03-14 22:00:01 +0000

    File changes

--- src/pages/PublicProfile.jsx
+++ src/pages/PublicProfile.jsx
@@ -367,7 +367,7 @@ export default function PublicProfile() {
                             {collectionGeckos.length > 0 ? (
                                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                                     {collectionGeckos.map(gecko => (
-                                        <GeckoCard key={gecko.id} gecko={gecko} isOwner={false} onView={(g) => navigate(createPageUrl(`GeckoDetail?id=${g.id}`))} onEdit={() => {}} />
+                                        <GeckoCard key={gecko.id} gecko={gecko} weightRecords={weightRecords} isOwner={false} onView={(g) => navigate(createPageUrl(`GeckoDetail?id=${g.id}`))} onEdit={() => {}} />
                                     ))}
                                 </div>
                             ) : (
