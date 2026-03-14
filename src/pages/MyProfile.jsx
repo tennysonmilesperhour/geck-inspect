c2555fbd6e6243e7f0b6dfb8d14ce51a5a346899
Tennyson Taggart <tennysontaggart@gmail.com>
2026-03-14 21:57:21 +0000

    File changes

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -656,7 +656,7 @@ export default function MyProfile() {
                                     {collectionGeckos.length > 0 ? (
                                         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                                             {collectionGeckos.map(gecko => (
-                                                <GeckoCard key={gecko.id} gecko={gecko} isOwner={true} onView={(g) => navigate(createPageUrl(`GeckoDetail?id=${g.id}`))} onEdit={() => navigate(createPageUrl('MyGeckos'))} />
+                                                <GeckoCard key={gecko.id} gecko={gecko} weightRecords={weightRecords} isOwner={true} onView={(g) => navigate(createPageUrl(`GeckoDetail?id=${g.id}`))} onEdit={() => navigate(createPageUrl('MyGeckos'))} />
                                             ))}
                                         </div>
                                     ) : (
